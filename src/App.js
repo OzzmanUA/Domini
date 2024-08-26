c7be6f3f9b8bb430687050c65514103d3a2cc1fb
HAZARD DUTY PAY! <odfoytch@gmail.com>
2024-08-26 12:05:20 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "/node_modules/bootstrap/dist/js/bootstrap.min.js";
+import "./App.css";
+import NavBar from "./component/common/NavBar";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+} from "react-router-dom";
+import Login from "./component/auth/Login"
+import Registration from "./component/auth/Registration"
+import { AuthProvider } from "./component/auth/AuthProvider"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container mt-5">
+			<AuthProvider>
+				<Router>
+					<NavBar />
+					<Routes>
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Registration />} />
+					</Routes>
+				</Router>
+			</AuthProvider>
+		</main>
   );
 }
 
